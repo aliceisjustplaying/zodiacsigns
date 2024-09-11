8ebdbda8079db4b0ed5037ae07927661610ef366
alice <aliceisjustplaying@gmail.com>
2024-09-11 18:33:09 +0100

    AAAAAAAAAAAAAAAAA

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const subscribe = () => {
   let cursor = 0;
   let intervalID: NodeJS.Timeout;
   const cursorFile = fs.readFileSync('cursor.txt', 'utf8');
-  const relay = cursorFile ? RELAY.concat('&cursor=', cursorFile) : RELAY;
+  const relay = cursorFile ? RELAY.concat('?cursor=', cursorFile) : RELAY;
   console.log(`Connecting to ${relay}`);
   const ws = new WebSocket(relay);
   if (cursorFile) console.log(`Initiate firehose at cursor ${cursorFile}`);
