cf73a5c598376410d036baefef6a60b1b9f015e3
alice <aliceisjustplaying@gmail.com>
2024-09-11 18:23:05 +0100

    wtf

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ import WebSocket from 'ws';
 const subscribe = () => {
   let cursor = 0;
   let intervalID: NodeJS.Timeout;
-  const cursorFile = fs.readFileSync("cursor.txt", "utf8");
-  const relay = cursorFile ? RELAY.concat("&cursor=", cursorFile) : RELAY;
+  const cursorFile = fs.readFileSync('cursor.txt', 'utf8');
+  const relay = cursorFile ? RELAY.concat('&cursor=', cursorFile) : RELAY;
   console.log(`Connecting to ${relay}`);
   const ws = new WebSocket(relay);
   if (cursorFile) console.log(`Initiate firehose at cursor ${cursorFile}`);
@@ -38,7 +38,8 @@ const subscribe = () => {
     if (data instanceof Buffer) {
       const event: EventStream = JSON.parse(data.toString()) as EventStream;
       cursor = event.time_us;
-      if (event.commit?.record.subject.uri.includes(`${DID}/app.bsky.feed.post`)) {
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      if (event.commit?.record?.subject?.uri?.includes(`${DID}/app.bsky.feed.post`)) {
         label(event.did, event.commit.record.subject.uri.split('/').pop()!).catch((err: unknown) => {
           console.error(err);
         });
